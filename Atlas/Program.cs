de95469662ccc62f7a9eae223d14a93d8efa6e3b
Odinson137 <buryy137@gmail.com>
2025-04-22 19:52:36 +0300

    add multy paging, add door music after successfull search

--- Atlas/Program.cs
+++ Atlas/Program.cs
@@ -21,6 +21,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
+app.UseStaticFiles();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
--- Atlas/obj/Debug/net8.0/Atlas.AssemblyInfo.cs
+++ Atlas/obj/Debug/net8.0/Atlas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atlas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe6edfe52864b4c20600263b0607f32b7cdad9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa63da4746160f0c92bc891df23b98831b2d21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atlas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atlas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
